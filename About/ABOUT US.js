3e7c011b3bcfcdd149b923ca0ce75a8e74b2e6c2
19Vako <frymendzordan@gmail.com>
2024-07-28 16:21:53 +0300

    add about us

---
+++ About/ABOUT US.js
@@ -0,0 +1,19 @@
+const miniNavLinks = document.getElementById("mini-nav-links");
+const miniMenuButton = document.getElementById("menu-button");
+
+miniMenuButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (miniNavLinks.style.display === "flex") {
+    miniNavLinks.style.display = "none";
+  } else {
+    miniNavLinks.style.display = "flex";
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (miniNavLinks.style.display === "flex" &&
+      !miniNavLinks.contains(event.target) &&
+      !miniMenuButton.contains(event.target)) {
+    miniNavLinks.style.display = "none";
+  }
+});
